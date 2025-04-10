80e6056c2a4f37768ccf44bdf55727fefb8a6fca
1eehyerin <danbe8080@gmail.com>
2025-04-10 15:34:28 +0900

    간단한 웹페이지 구조와 mysql + mariadb연동

---
+++ server.js
@@ -0,0 +1,19 @@
+let http = require('http');
+let url = require('url');
+
+function start(route, handle) {
+    function onRequest(request, response) {
+        /* 
+        favicon.ico error 
+        favicon을 찾으려고 해서 나는 에러
+        해결 방안으로 그냥 요청을 무시하게끔 한다.
+        */ 
+        if(request.url.includes("favicon.ico")) return;
+        let pathname = url.parse(request.url).pathname;
+        route(pathname, handle, response);
+    }
+    
+    http.createServer(onRequest).listen(8888);
+}
+
+exports.start = start;
